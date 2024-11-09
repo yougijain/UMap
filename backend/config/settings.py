0f6555b78d68b6f39b3f1bdb9b8492c7e438a754
yougijain <yougijain@gmail.com>
2024-11-09 17:30:37 -0500

    adding requirements.txt and adding domain names

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-z(0ch81k*l-s+8_t66+m1tfy1wnrpm1vb181ha!k3fwb-x8mvv
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['yourumap.onrender.com', 'yourumap.tech']
 
 
 # Application definition
