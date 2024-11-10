cc380f908f422f2669e9808be844a3f8c1b52597
yougijain <yougijain@gmail.com>
2024-11-10 04:17:02 -0500

    fixed login page

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['yourumap.onrender.com', 'umap-uglf.onrender.com', 'yourumap.tech','www.yourumap.tech', '127.0.0.1', 'localhost']
 
