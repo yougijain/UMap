d2a730ec8e9571fc1b2ee91946dcf812dd2e40d7
yougijain <yougijain@gmail.com>
2024-11-10 15:53:45 -0500

    privacy fixes

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", "fallback-secret-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['yourumap.onrender.com', 'umap-uglf.onrender.com', 'yourumap.tech','www.yourumap.tech', '127.0.0.1', 'localhost']
 
