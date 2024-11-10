2a81f1f25d9f28beabef081323a5212d64802429
yougijain <yougijain@gmail.com>
2024-11-09 20:26:51 -0500

    retest homepage bug fix

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, '../../frontend/Homepage')],
+        'DIRS': [os.path.join(BASE_DIR, '../../../frontend/Homepage')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
